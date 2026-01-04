4d5dde71190000117e89232bb15c790fb136b271
Hariharan <hariharan9572@gmail.com>
2026-01-05 01:21:08 +0530

    First Spring Boot App

---
+++ src/main/java/com/hariharan/app/Alien.java
@@ -0,0 +1,9 @@
+package com.hariharan.app;
+
+public class Alien {
+
+    public void code(){
+        System.out.println("Coding");
+    }
+
+}
---
+++ src/main/java/com/hariharan/app/SpringBootFirstApplication.java
@@ -0,0 +1,17 @@
+package com.hariharan.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFirstApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootFirstApplication.class, args);
+
+        Alien obj;
+        obj.code();
+
+    }
+
+}
