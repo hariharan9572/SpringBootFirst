204975c90eda5513e1ca047ea7409529d77e0f24
Hariharan <hariharan9572@gmail.com>
2026-01-05 01:27:43 +0530

    DI Using Spring Boot

--- src/main/java/com/hariharan/app/SpringBootFirstApplication.java
+++ src/main/java/com/hariharan/app/SpringBootFirstApplication.java
@@ -2,15 +2,19 @@ package com.hariharan.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringBootFirstApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootFirstApplication.class, args);
+        ApplicationContext context = SpringApplication.run(SpringBootFirstApplication.class, args);
 
-        Alien obj;
+        Alien obj = context.getBean(Alien.class);
         obj.code();
+
+        Alien obj1 = context.getBean(Alien.class);
+        obj1.code();
 
     }
 
